056a483415efa9090169e9f9acc931f59abbb71c
01Joseph-Hwang10 <joseph95501@gmail.com>
2023-08-30 01:24:24 +0900

    fix: newyork dark image in dark mode

--- src/page-contents/Home/Header/Hero.tsx
+++ src/page-contents/Home/Header/Hero.tsx
@@ -1,22 +1,12 @@
 import React from "react";
-import { useColorMode } from "@docusaurus/theme-common";
 import clsx from "clsx";
 import styles from "./styles.module.scss";
 
 const HomepageHero: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const { colorMode } = useColorMode();
-  const imageUri =
-    colorMode === "dark" ? `/img/newyork-dark.jpg` : `/img/newyork-light.jpg`;
-
   return (
-    <header
-      className={clsx("hero hero--primary", styles.hero)}
-      style={{
-        backgroundImage: `url(${imageUri})`,
-      }}
-    >
+    <header className={clsx("hero hero--primary", styles.hero)}>
       {children}
     </header>
   );
