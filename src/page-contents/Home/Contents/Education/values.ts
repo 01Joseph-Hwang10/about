3c91e88cee7eae60c58afc7234675a7749944b0e
01Joseph-Hwang10 <joseph95501@gmail.com>
2023-08-30 23:00:47 +0900

    chore: 재학 중 -> 휴학 중

--- src/page-contents/Home/Contents/Education/values.ts
+++ src/page-contents/Home/Contents/Education/values.ts
@@ -7,7 +7,7 @@ const educations: Education[] = [
     id: "UNIST",
     title: "울산과학기술원",
     details: ["컴퓨터공학과 학사과정 (주전공)", "경영과학부 학사과정 (부전공)"],
-    period: "2021.03 - 재학 중",
+    period: "2021.03 - 휴학 중",
     onClick: () => {
       if (typeof window === "undefined") return;
       window.open("https://www.unist.ac.kr/", "_blank");
