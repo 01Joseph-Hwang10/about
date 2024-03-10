90a938d24f9df18eaa80cae080704175c15bb656
01Joseph-Hwang10 <joseph95501@gmail.com>
2024-03-11 04:08:32 +0900

    chore: disable progressive loading

--- src/page-contents/Home/Header/Hero.tsx
+++ src/page-contents/Home/Header/Hero.tsx
@@ -1,21 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 import clsx from "clsx";
 // @ts-ignore
 import styles from "./styles.module.scss";
-import useIsDarkMode from "@site/src/hooks/useIsDarkMode";
 
 const HomepageHero: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const isDarkMode = useIsDarkMode();
-
-  useEffect(() => {
-    const header = document.getElementById("homepage-hero");
-    header.style.backgroundImage = isDarkMode
-      ? `url("/img/newyork-dark.jpg")`
-      : `url("/img/newyork-light.jpg")`;
-  }, [isDarkMode]);
-
   return (
     <header
       id="homepage-hero"
